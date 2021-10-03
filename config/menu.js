6e3939acb07630b438fe02eac54b3c358003a671
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2021-10-03 11:34:02 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,11 +1,18 @@
 const menu = [
   { type: 'config', id: 'news' },
   // { type: 'config', id: 'live' },
-  // { type: 'config', id: 'tickets' },
-  // { type: 'config', id: 'tournaments' },
-  { type: 'config', id: 'info' }
+  { type: 'config', id: 'tickets' },
+  { type: 'config', id: 'tournaments' },
+  { type: 'config', id: 'info' },
   // { type: 'page', title: 'Animations', id: 4012, link: '/animations' },
-  // { type: 'external', title: '2018', link: 'https://2018.atlantic-lan.com' }
+  {
+    type: 'nolink',
+    title: 'Archives',
+    children: [
+      { type: 'external', title: '2019', link: 'https://2019.atlantic-lan.com' },
+      { type: 'external', title: '2018', link: 'https://2018.atlantic-lan.com' }
+    ]
+  }
 ]
 
 module.exports = menu
